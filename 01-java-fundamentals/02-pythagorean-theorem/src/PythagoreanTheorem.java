a331e4e822a9b541eb9ca36a05a195c4a0237162
adegbola <akolade4real@gmail.com>
2021-01-13 16:46:03 -0600

    hw

---
+++ 01-java-fundamentals/02-pythagorean-theorem/src/PythagoreanTheorem.java
@@ -0,0 +1,8 @@
+public class PythagoreanTheorem {
+
+    public static final int POWER = 2;
+
+    public double pythagorean(int a, int b) {
+        return Math.sqrt(Math.pow(a, POWER) + Math.pow(b, POWER));
+    }
+}
---
+++ 01-java-fundamentals/02-pythagorean-theorem/src/main.java
@@ -0,0 +1,9 @@
+public class main {
+    public static void main(String[] args) {
+        PythagoreanTheorem pythagoreanTheorem = new PythagoreanTheorem();
+        double result = pythagoreanTheorem.pythagorean(5, 5);
+        System.out.println(result);
+    }
+
+
+}
